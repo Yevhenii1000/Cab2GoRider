b8f989a74b79dc7c08517a09cbe21bd302e923d3
Yevhenii <yevhenii@macbook-pro-yevhenii.local>
2018-04-18 21:24:30 +0300

    Initial Commit

---
+++ Cab2GoRider/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Cab2GoRider
+//
+//  Created by Yevhenii on 18.04.2018.
+//  Copyright © 2018 Yevhenii. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func sugnUpButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
