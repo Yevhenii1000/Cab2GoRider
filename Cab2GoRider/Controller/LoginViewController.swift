d3a677b78657e3834a9466bacf2fb72564d35342
Yevhenii <yevhenii@macbook-pro-yevhenii.local>
2018-04-25 11:45:07 +0300

    Login Error Handling Completed

--- Cab2GoRider/Controller/LoginViewController.swift
+++ Cab2GoRider/Controller/LoginViewController.swift
@@ -17,8 +17,34 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    //MARK:- Other Methods
+    
+    private func alertUser(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alerAction_OK = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(alerAction_OK)
+        present(alert, animated: true, completion: nil)
+    }
     
     @IBAction func signInButtonPressed(_ sender: UIButton) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            
+            UsersAuthorizationManager.authManager.login(email: emailTextField.text!, password: passwordTextField.text!, loginHandler: { message in
+                
+                if message != nil {
+                    
+                    self.alertUser(title: "Authentication Error", message: message!)
+                    
+                } else {
+                    
+                    print("Login Completed")
+                    
+                }
+                
+            })
+            
+        }
     }
     
     @IBAction func sugnUpButtonPressed(_ sender: UIButton) {
---
+++ Cab2GoRider/Supporting/UsersAuthorizationManager.swift
@@ -0,0 +1,80 @@
+//
+//  UsersAuthenticationManager.swift
+//  Cab2GoRider
+//
+//  Created by Yevhenii on 22.04.2018.
+//  Copyright © 2018 Yevhenii. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+typealias LoginHandler = (_ message: String?) -> Void
+
+struct LoginErrorCodeMessage {
+    static let invalidEmailMessage = "Invalid Email Address. Please, Provide an Valid Email Address"
+    static let wrongPasswordMessage = "Wrong Password. Please, Enter The Correct Password"
+    static let problemConnectingMessage = "Cannot Connect to Database. Please, Try Again Later"
+    static let userNotFoundMessage = "User Not Found. Please, Sign Up"
+    static let emailAlreadyInUseMessage = "Email Is Already In Use. Please, Use Another Email"
+    static let weakPasswordMessage = "Password Should Be At Least 6 Characters Long"
+}
+
+class UsersAuthorizationManager {
+    
+    private static let instance = UsersAuthorizationManager()
+    
+    static var authManager: UsersAuthorizationManager {
+        
+        return instance
+        
+    }
+    
+    func login(email email: String, password: String, loginHandler: LoginHandler?) {
+        
+        Auth.auth().signIn(withEmail: email, password: password, completion: { user, error in
+            
+            if error != nil {
+                
+                self.handleErrors(error: error! as NSError, loginHandler: loginHandler)
+                
+            } else {
+                
+                loginHandler?(nil)
+                
+            }
+            
+            //Handle user authorization
+            
+        })
+        
+    }
+    
+    private func handleErrors(error: NSError, loginHandler: LoginHandler?) {
+        
+        if let errorCode = AuthErrorCode(rawValue: error.code) {
+            
+            switch errorCode {
+                
+            case .wrongPassword:
+                loginHandler?(LoginErrorCodeMessage.wrongPasswordMessage)
+            case .invalidEmail:
+                loginHandler?(LoginErrorCodeMessage.invalidEmailMessage)
+            case .userNotFound:
+                loginHandler?(LoginErrorCodeMessage.userNotFoundMessage)
+            case .emailAlreadyInUse:
+                loginHandler?(LoginErrorCodeMessage.emailAlreadyInUseMessage)
+            case .weakPassword:
+                loginHandler?(LoginErrorCodeMessage.weakPasswordMessage)
+            default:
+                loginHandler?(LoginErrorCodeMessage.problemConnectingMessage)
+                
+            }
+            
+        }
+        
+    }
+    
+    
+}
+
